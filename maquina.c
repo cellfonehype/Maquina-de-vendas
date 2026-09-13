b65e5029258357d0ff93a926850345920fd8a7d5
pedro <pedromardegan825@gmail.com>
2026-09-13 19:38:42 -0300

    quantidades e valores adicionados

--- maquina.c
+++ maquina.c
@@ -4,8 +4,12 @@
 int main()
 {
     char produtos[8][30] = {
-        "Coca, Guarana, Pepsi, Agua, Doritos, Batata Chips, Barra de chocolate, Red Bull"}
-
+        "Coca, Guarana, Pepsi, Agua, Doritos, Batata Chips, Barra de chocolate, Red Bull"};
+    int quantidade[8] = {
+        9,5,2,5,10,5,12,7};
+    int preco[8] = {
+        6,5,6,3,7,5,4,8};
+    
 
 
 }

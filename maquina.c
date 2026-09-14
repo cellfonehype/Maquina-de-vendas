dcc29886d31d26fa14a695350d457a8ea2e7f4be
pedro <pedromardegan825@gmail.com>
2026-09-13 22:49:17 -0300

    começo do sistema

--- maquina.c
+++ maquina.c
@@ -3,16 +3,39 @@
 
 int main()
 {
+    //layout da maquina de vendas
     char produtos[8][30] = {
-        "Coca", "Guarana", "Pepsi", "Agua", "Doritos", "Batata Chips", "Barra de chocolate", "Red Bull"};
+        "1-Coca", "2-Guarana", "3-Pepsi", "4-Agua", "5-Doritos", "6-Batata Chips", "7-Barra de chocolate", "8-Red Bull"};
     int quantidade[8] = {
         9,5,2,5,10,5,12,7};
     int preco[8] = {
         6,5,6,3,7,5,4,8};
+
+    //sistema de compra
+    int opcao;
     
+
+    printf("Bem vindo\nDigite oque vc deseja:\n");
     for(int i = 0;i<8;i++)
     {
-        printf("produto: %s | preço: R$%d | quantidade: %d |\n", produtos[i], preco[i], quantidade[i]);
+        printf("produto: %s      | preço: R$%d |      quantidade: %d |\n", produtos[i], preco[i], quantidade[i]);
+
     }
+
+
+
+    do{
+        scanf("%d", &opcao);
+        opcao--;
+
+        if(opcao > 0 && opcao < 8 && preco[opcao] != 0)
+        {
+            printf("vc esta aqui");
+
+        }
+
+    }while(opcao != -1);
+
+
 return 0;
 }

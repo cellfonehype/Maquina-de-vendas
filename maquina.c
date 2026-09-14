11d9420c258f9a2ce5c6a3e35d1f566c5e39d793
pedro <pedromardegan825@gmail.com>
2026-09-13 22:20:14 -0300

    layout da maquina de vendas

--- maquina.c
+++ maquina.c
@@ -4,12 +4,15 @@
 int main()
 {
     char produtos[8][30] = {
-        "Coca, Guarana, Pepsi, Agua, Doritos, Batata Chips, Barra de chocolate, Red Bull"};
+        "Coca", "Guarana", "Pepsi", "Agua", "Doritos", "Batata Chips", "Barra de chocolate", "Red Bull"};
     int quantidade[8] = {
         9,5,2,5,10,5,12,7};
     int preco[8] = {
         6,5,6,3,7,5,4,8};
     
-
-
+    for(int i = 0;i<8;i++)
+    {
+        printf("produto: %s | preço: R$%d | quantidade: %d |\n", produtos[i], preco[i], quantidade[i]);
+    }
+return 0;
 }

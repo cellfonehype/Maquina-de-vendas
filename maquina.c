05c41ac473b26423c407a18803031c4b46f6647f
pedro <pedromardegan825@gmail.com>
2026-09-13 23:44:38 -0300

    escolha de itens 1.2

--- maquina.c
+++ maquina.c
@@ -7,7 +7,7 @@ int main()
     char produtos[8][30] = {
         "1-Coca", "2-Guarana", "3-Pepsi", "4-Agua", "5-Doritos", "6-Batata Chips", "7-Barra de chocolate", "8-Red Bull"};
     int quantidade[8] = {
-        9,5,2,5,10,5,12,7};
+        9,2,2,5,10,5,12,7};
     int preco[8] = {
         6,5,6,3,7,5,4,8};
 
@@ -23,18 +23,22 @@ int main()
     }
 
 
-
     do{
         scanf("%d", &opcao);
-        opcao--;
-
-        if(opcao > 0 && opcao < 8 && preco[opcao] != 0)
+        
+        if(opcao <= -1)
         {
-            printf("vc esta aqui");
+            printf("vc saiu ate a proxima!!");
+            break;
+        }
+        opcao--;
+        if(opcao >= 0 && opcao < 8 && quantidade[opcao] != 0)
+        {
+            printf("vc escolheu: %s | R$:%d",produtos[opcao], preco[opcao]);
 
         }
 
-    }while(opcao != -1);
+    }while(1);
 
 
 return 0;

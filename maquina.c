5a810f756673bdd65d2ed61826e35e587a9b56b3
pedro <pedromardegan825@gmail.com>
2026-09-14 00:12:20 -0300

    maquina finalizada

--- maquina.c
+++ maquina.c
@@ -12,7 +12,7 @@ int main()
         6,5,6,3,7,5,4,8};
 
     //sistema de compra
-    int opcao;
+    int opcao,pagamento,troco,denovo;
     
 
     printf("Bem vindo\nDigite oque vc deseja:\n");
@@ -24,22 +24,60 @@ int main()
 
 
     do{
+        printf("Escolha:\n");
         scanf("%d", &opcao);
-        
+        opcao--;
         if(opcao <= -1)
         {
             printf("vc saiu ate a proxima!!");
             break;
         }
-        opcao--;
         if(opcao >= 0 && opcao < 8 && quantidade[opcao] != 0)
         {
-            printf("vc escolheu: %s | R$:%d",produtos[opcao], preco[opcao]);
+            printf("vc escolheu: %s | R$:%d\n",produtos[opcao], preco[opcao]);
+            printf("Deposite o valor:");
+            scanf("%d", &pagamento);
 
+                if(pagamento >= preco[opcao])
+                {   
+                    quantidade[opcao]--;
+                    troco = pagamento - preco[opcao];
+                    printf("Deposito aceito!!\nVc recebeu R$:%d de troco\n",troco);
+
+                }
+                else if(pagamento < preco[opcao])
+                {
+                    troco = preco[opcao] - pagamento;
+                    printf("Dinheiro insuficiente!!\nfalta R$%d\n", troco);
+
+                }
+
+                printf("deseja comprar denovo:\n1-sim\n2-não\n");
+                scanf("%d", &denovo);
+
+
+                if(denovo == 1)
+                {
+                        for(int i = 0;i<8;i++)
+                        {
+                        printf("produto: %s      | preço: R$%d |      quantidade: %d |\n", produtos[i], preco[i], quantidade[i]);
+                            continue;
+                        }
+                }
+                else
+                {
+                    break;
+                }
+        }
+        else if(quantidade[opcao] == 0 )
+        {
+            printf("Esse item esta fora de estoque\nEscolha outro ou saia com 0");
+            continue;
         }
 
-    }while(1);
 
+    }while(1);
+printf("vc saiu ate a proxima!!");
 
 return 0;
 }
